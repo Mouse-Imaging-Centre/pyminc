0675b8a4fc287662d9d84c92c0af7718424643eb
Jason Lerch <jason@phenogenomics.ca>
2009-05-04 14:19:03 -0400

    added __init__.py, bumped version to 0.1.1

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 
 setup(name='pyminc',
-      version = '0.1',
+      version = '0.1.1',
+      author = "Jason Lerch",
+      author_email = "jason@phenogenomics.ca",
       packages = ['pyminc', 'pyminc.volumes'],
       )
