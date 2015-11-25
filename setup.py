55ad375851cf5cf7509bdcddc87c631e89231691
Ben Darwin <bcdarwin@gmail.com>
2015-11-25 11:17:58 -0500

    move tests to their own dir and update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='pyminc',
       version = '0.41',
+      description = "Python interface to libminc",
+      url = "https://github.com/Mouse-Imaging-Centre/pyminc",
       author = "Jason Lerch",
       author_email = "jason@mouseimaging.ca",
-      packages = ['pyminc', 'pyminc.volumes'],
+      license = "BSD",
+      packages = find_packages(), #['pyminc', 'pyminc.volumes'],
+      install_requires = ["numpy"],
       scripts = ['scripts/sva2mnc.py'],
+      test_suite="test",
       )
