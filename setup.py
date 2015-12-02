b4544bc4eb6af4ebd49c52e0f301246d7c834580
Ben Darwin <bcdarwin@gmail.com>
2015-12-02 14:42:50 -0500

    new version; setup.py fixes

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='pyminc',
-      version = '0.41',
+      version = '0.42b',
       description = "Python interface to libminc",
       url = "https://github.com/Mouse-Imaging-Centre/pyminc",
       author = "Jason Lerch",
       author_email = "jason@mouseimaging.ca",
       license = "BSD",
-      packages = find_packages(), #['pyminc', 'pyminc.volumes'],
+      packages = find_packages(),
       install_requires = ["numpy"],
-      scripts = ['scripts/sva2mnc.py'],
+      scripts = ["scripts/sva2mnc.py", "pyminc_test2.py"],
       test_suite="test",
       )
