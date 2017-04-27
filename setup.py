8543e8006d5d38649d9de877208d3c06abea34a5
Matthijs van Eede <matthijsvaneede@gmail.com>
2017-04-27 15:56:52 -0400

    updated the NEWS and the version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyminc',
-      version = '0.50',
+      version = '0.51',
       description = "Python interface to libminc",
       url = "https://github.com/Mouse-Imaging-Centre/pyminc",
       author = "Jason Lerch",
