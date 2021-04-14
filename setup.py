4f86e0c3bdf7b94801d8582996354b2d7310d0a4
Ben Darwin <bcdarwin@gmail.com>
2021-04-14 14:41:21 -0400

    bump version and edit NEWS

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pyminc',
-      version = '0.53.3',
+      version = '0.54',
       description = "Python interface to libminc",
       url = "https://github.com/Mouse-Imaging-Centre/pyminc",
       author = "Jason Lerch",
