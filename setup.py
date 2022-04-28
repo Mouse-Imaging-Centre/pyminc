4e90b697e3b1a5258e7489b7545f4c7dc25524bc
Ben Darwin <bcdarwin@gmail.com>
2022-04-28 13:15:04 -0400

    bump version and edit NEWS

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='pyminc',
-      version = '0.55',
+      version = '0.56',
       description = "Python interface to libminc",
       url = "https://github.com/Mouse-Imaging-Centre/pyminc",
       author = "Jason Lerch",
       license = "BSD",
       packages = find_packages(exclude=["test"]),
-      python_requires = ">=3.5",
+      python_requires = ">=3.7",
       install_requires = ["numpy"],
       scripts = ["scripts/sva2mnc.py", "scripts/pyminc_test2.py"],
       tests_require = ["pytest", "parameterized"],
